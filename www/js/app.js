8db10d4d8c65bbf9275c6d4a7b335e0bba637c3c
nsarychev15 <nikolay.sarychev@mapquest.com>
2015-07-11 12:03:18 -0600

    Chanfing configuration.

--- www/js/app.js
+++ www/js/app.js
@@ -1,83 +1,82 @@
-// Ionic Starter App
+angular.module('ionicApp', ['ionic'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+    .config(function($stateProvider, $urlRouterProvider) {
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
-  });
-})
+      $stateProvider
+          .state('search', {
+            url: '/search',
+            templateUrl: 'search.html'
+          })
+          .state('settings', {
+            url: '/settings',
+            templateUrl: 'settings.html'
+          })
+          .state('tabs', {
+            url: "/tab",
+            abstract: true,
+            templateUrl: "tabs.html"
+          })
+          .state('tabs.home', {
+            url: "/home",
+            views: {
+              'home-tab': {
+                templateUrl: "home.html",
+                controller: 'HomeTabCtrl'
+              }
+            }
+          })
+          .state('tabs.facts', {
+            url: "/facts",
+            views: {
+              'home-tab': {
+                templateUrl: "facts.html"
+              }
+            }
+          })
+          .state('tabs.facts2', {
+            url: "/facts2",
+            views: {
+              'home-tab': {
+                templateUrl: "facts2.html"
+              }
+            }
+          })
+          .state('tabs.about', {
+            url: "/about",
+            views: {
+              'about-tab': {
+                templateUrl: "about.html"
+              }
+            }
+          })
+          .state('tabs.navstack', {
+            url: "/navstack",
+            views: {
+              'about-tab': {
+                templateUrl: "nav-stack.html"
+              }
+            }
+          })
+          .state('tabs.contact', {
+            url: "/contact",
+            views: {
+              'contact-tab': {
+                templateUrl: "contact.html"
+              }
+            }
+          });
 
-.config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+      $urlRouterProvider.otherwise("/tab/home");
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
+    .controller('NavCtrl', function($scope, $ionicSideMenuDelegate) {
+      $scope.showMenu = function () {
+        $ionicSideMenuDelegate.toggleLeft();
+      };
+      $scope.showRightMenu = function () {
+        $ionicSideMenuDelegate.toggleRight();
+      };
     })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
-});
+    .controller('HomeTabCtrl', function($scope) {
+    });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,12 @@
-angular.module('starter.controllers', [])
+angular.module('ionicApp.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
-
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-  
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+    .controller('NavCtrl', function($scope, $ionicSideMenuDelegate) {
+      $scope.showMenu = function () {
+        $ionicSideMenuDelegate.toggleLeft();
+      };
+      $scope.showRightMenu = function () {
+        $ionicSideMenuDelegate.toggleRight();
+      };
+    })
+    .controller('HomeTabCtrl', function($scope) {
+    })
