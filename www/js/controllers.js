0a10cef25987cd4c33c3a69cdeb527c0eed1bd10
nsarychev15 <nikolay.sarychev@mapquest.com>
2015-07-14 00:38:02 -0600

    Adding social.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -70,4 +70,19 @@ angular.module('AutoGuru.controllers', [])
 
       }
 
-    }]);
+    }])
+    .controller("ExampleController", function($scope, $cordovaSocialSharing) {
+
+      $scope.shareAnywhere = function() {
+        $cordovaSocialSharing.share("This is your message", "This is your subject", "www/imagefile.png", "http://blog.nraboy.com");
+      }
+
+      $scope.shareViaTwitter = function(message, image, link) {
+        $cordovaSocialSharing.canShareVia("twitter", message, image, link).then(function(result) {
+          $cordovaSocialSharing.shareViaTwitter(message, image, link);
+        }, function(error) {
+          alert("Cannot share on Twitter");
+        });
+      }
+
+    });
